7f065cbca7e8e8ab5bf687f82c736c55c2557710
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-26 17:07:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+ my_hash.new
   
 end
-
+ 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
